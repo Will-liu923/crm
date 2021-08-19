675b2c712d91d5b463efc502d40fe66237a12263
Will Liu <leoLW923@outlook.com>
2021-08-19 21:19:52 +0800

    test branch dev

--- src/main/java/top/sakura/crm/pojo/Test.java
+++ src/main/java/top/sakura/crm/pojo/Test.java
@@ -6,4 +6,9 @@ package top.sakura.crm.pojo;
  * @create 2021-08-19 21:15
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
 }
