532bd7377a1bb9679e239883141d7e316247782a
Will Liu <leoLW923@outlook.com>
2021-08-19 21:21:44 +0800

    test branch master

--- src/main/java/top/sakura/crm/pojo/Test.java
+++ src/main/java/top/sakura/crm/pojo/Test.java
@@ -6,4 +6,10 @@ package top.sakura.crm.pojo;
  * @create 2021-08-19 21:15
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+    }
 }
